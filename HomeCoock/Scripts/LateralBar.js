b0c079917615eaab1b4c130eb4f68c2fe6b8c204
JohnDev <con.j.de.soria@outlook.com>
2018-10-03 17:31:10 +0200

    Updated lateral bar

--- HomeCoock/Scripts/LateralBar.js
+++ HomeCoock/Scripts/LateralBar.js
@@ -1,25 +1,13 @@
 ﻿$(function () {
-    $('#navigation > li').hover(
-        function () {
-            $('a', $(this)).stop().animate({ 'marginLeft': '-2px' }, 200);
-        },
-        function () {
-            $('a', $(this)).stop().animate({ 'marginLeft': '-85px' }, 200);
-        }
-    );
-});
-
-$(function () {
 
     $('#navigation a').stop().animate({ 'marginLeft': '-85px' }, 1000);
 
-    $('#navigation > li').hover(
+    $('#navigation li a').hover(
         function () {
-            $('a', $(this)).stop().animate({ 'marginLeft': '-2px' }, 200);
+            $($(this)).stop().animate({ 'marginLeft': '-2px' }, 200);
         },
         function () {
-            $('a', $(this)).stop().animate({ 'marginLeft': '-85px' }, 200);
+            $($(this)).stop().animate({ 'marginLeft': '-85px' }, 200);
         }
     );
 });
-
