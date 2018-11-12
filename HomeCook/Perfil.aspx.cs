bc588db9f5282007dc2152c74dd888387d558e41
Juanan <con.j.de.soria@outlook.com>
2018-11-12 16:57:46 +0100

    Error correction

--- HomeCook/Perfil.aspx.cs
+++ HomeCook/Perfil.aspx.cs
@@ -1,13 +1,7 @@
 ﻿using HomeCook.Clases;
 using Microsoft.Data.Sqlite;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.HtmlControls;
-using System.Web.UI.WebControls;
 
 namespace HomeCook
 {
